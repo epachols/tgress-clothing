80ad83a647e5147ccd90d6f98a5461e3ed379ab9
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-09-17 11:17:22 -0700

    styled menu items to completion, added overflow-hidden

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,9 +3,10 @@ import "./menu-item.styles.scss"
 
 export default function MenuItem({title, size, imageUrl}) {
     return (
-        <div style={{
+        <div className={`${size} menu-item`}>
+        <div className='background-image' style={{
             backgroundImage: `url(${imageUrl})`
-        }} className={`${size} menu-item`}>
+        }}></div>
             <div className="content">
               <h1 className="title">{title.toUpperCase()}</h1>
               <span className="subtitle">browse now</span>
