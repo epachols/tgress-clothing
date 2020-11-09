1e1230c355d7a5357d8eded68f1b42cc63b09327
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-11-09 11:21:45 -0800

    added show-hide function to cart with a cart reducer and a hidden prop, mapped

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <>
     <div className="header">
@@ -35,14 +35,16 @@ const Header = ({ currentUser }) => {
         <CartIcon />
 
       </div>
-      <CartDropdown />
+      { hidden ? null : <CartDropdown /> }
     </div>
       </>
   );
 }
 
-const mapStateToProps = state =>({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: { hidden } }) =>({
+  currentUser,
+  hidden
+
 });
 
 export default connect(mapStateToProps)(Header);
