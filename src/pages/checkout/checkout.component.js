fb4c5193acfa16096a841029399eefe53e8995e6
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-11-17 11:54:12 -0800

    added checkout-item component skeleton, imported into checkout page and updated reselect in cart

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import {
   selectCartItems,
   selectCartTotal,
@@ -35,7 +36,7 @@ const CheckoutPage = ({ cartItems, total }) => {
       </div>
     {
         cartItems.map(cartItem => 
-        cartItem.name
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         )
     }
     <div className="total">
