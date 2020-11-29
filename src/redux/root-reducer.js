f1ae4c4076248b7612b808f440f27641534e2155
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-11-29 09:15:52 -0800

    modified root reducer to add in persist capabilities

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import {combineReducers} from 'redux';
+import { persistReducer } from 'redux-persist';
+// saying we want to use localstorage, could also use session storage instead
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+// key is where we want to start our persistent storage
+// whitelist arr determines persist. for now we say cart only.
+const persistConfig = {
+    key: 'root',
+    storage,
+    whiteList: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer)
