f6e27cba843fca30d373f038a698617eff821868
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-12-02 15:29:34 -0800

    routed collection component properly, added in selector -> note: will need to use lodash memoize to memoize the whole selectCollection function, next

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
-import './collection.styles.scss';
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
-const CollectionPage = ({ match }) => {
-    return (
-        <div className="collection-page">
-            <h2>collection page</h2>
-        </div>
-    )
-}
+import "./collection.styles.scss";
 
-export default CollectionPage;
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  return (
+    <div className="collection-page">
+      <h2>collection page</h2>
+    </div>
+  );
+};
+
+// note- in order to map state to props here, we need to pass the second optional argument to map state to props as ownProps (the props belonging to the present component)
+const mapStateToProps = (state, ownProps) => {
+  return ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state),
+  });
+};
+
+export default connect(mapStateToProps)(CollectionPage);
