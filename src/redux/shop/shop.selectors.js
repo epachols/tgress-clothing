36d1dc2fdb5639dbd97208c63c31d70d964e3df0
Capitol Heel <66080081+epachols@users.noreply.github.com>
2021-02-17 11:17:37 -0800

    added loading spinner HOC

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -10,13 +10,12 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
-    collections => Object.keys(collections).map(key => collections[key])
+    collections => collections ? Object.keys(collections).map(key => collections[key]) : []
   );
   
   export const selectCollection = collectionUrlParam =>
     createSelector(
       [selectCollections],
-      collections => collections[collectionUrlParam]
+      collections => (collections? collections[collectionUrlParam] : null)
     );
 
-    // note the increasing specificity in the selector chain, and how it's presented.
